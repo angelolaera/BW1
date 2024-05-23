6d5d4e08bc8970f4f33374e5052a3f0f432e875f
angelolaera <angelo.laera.1995@gmail.com>
2024-05-23 14:49:53 +0200

    Ultimo aggiornamento grafico

--- Assets/js/results.js
+++ Assets/js/results.js
@@ -32,6 +32,9 @@ const chartData = {
 const myChart = document.querySelector(".my-chart");
 const ul = document.querySelector(".programming-stats .details ul");
 
+myChart.width = 400; // Larghezza desiderata del grafico
+myChart.height = 400;
+
 new Chart(myChart, {
   type: "doughnut",
   data: {
@@ -40,13 +43,17 @@ new Chart(myChart, {
       {
         label: "Percetuale",
         data: chartData.data,
+        backgroundColor: ["#00ffff", "rgb(175, 16, 127)"],
+        borderColor: ["#00ffff", "rgb(175, 16, 127)"],
+        rotation: 180,
       },
     ],
   },
   options: {
+    cutout: "80%", // Definiamo lo spessore delle barre interne
     responsive: true,
-    //borderWidth: 10,
-    borderRadius: 2,
+    //borderWidth: 5,
+    borderRadius: 0,
     hoverBorderWidth: 0,
     plugins: {
       legend: {
